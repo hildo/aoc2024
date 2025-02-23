1c4f252aa70e7b2fa0a6119f3627cf1d97a0cd1b
Ed Hillmann <edhillmann@yahoo.com>
2025-02-23 20:44:49 +1000

    Part two done

--- src/day05.rs
+++ src/day05.rs
@@ -57,9 +57,63 @@ fn middle_entry(pages: &Vec<u32>) -> u32 {
     pages[middle_idx]
 }
 
+fn correct_order(pages: &Vec<u32>, rules: &Vec<PageRule>) -> Vec<u32> {
+    let mut return_value = pages.clone();
+    let mut rule_idx = 0;
+    while rule_idx < rules.len() {
+        let rule = &rules[rule_idx];
+        if test_pages(&return_value, rule) {
+            rule_idx += 1;
+            continue;
+        }
+        let current_before_idx = return_value.iter().position(|&x| x == rule.before_page_num);
+        let current_after_idx = return_value.iter().position(|&x| x == rule.after_page_num);
+        match (current_before_idx, current_after_idx) {
+            (Some(before), Some(after)) => {
+                return_value.remove(before);
+                return_value.insert(after, rule.before_page_num);
+                rule_idx = 0;
+                continue;
+            }
+            _ => {
+                // nothing
+            }
+        }
+        rule_idx += 1;
+    }
+    return_value
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_part_two() {
+        let (rules, all_pages) = load_input("./src/resources/day05_input.txt");
+        let entry_sum: u32 = all_pages.iter()
+            .filter(|pages| !are_pages_ordered(pages, &rules))
+            .map(|pages| correct_order(pages, &rules))
+            .map(|pages| middle_entry(&pages))
+            .sum();
+        assert_eq!(4598, entry_sum);
+    }
+
+    #[test]
+    fn test_correct_order() {
+        let (rules, all_pages) = load_input("./src/resources/day05_simple.txt");
+        assert_eq!(vec![97,75,47,61,53], correct_order(&all_pages[3], &rules));
+        assert_eq!(vec![61,29,13], correct_order(&all_pages[4], &rules));
+        assert_eq!(vec![97,75,47,29,13], correct_order(&all_pages[5], &rules));
+
+        let entry_sum: u32 = all_pages.iter()
+            .filter(|pages| !are_pages_ordered(pages, &rules))
+            .map(|pages| correct_order(pages, &rules))
+            .map(|pages| middle_entry(&pages))
+            .sum();
+        assert_eq!(123, entry_sum);
+
+    }
 
     #[test]
     fn test_part_one() {
